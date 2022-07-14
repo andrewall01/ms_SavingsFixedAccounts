6c7435572c4db60720b5393e508f4b4c5baebdab
Andres Llontop <andresllontop01@gmail.com>
2022-07-14 18:53:27 -0500

    Se actualiza pom

--- src/main/java/com/project/ms/savings/fixed/accounts/MsSavingsFixedAccountsApplication.java
+++ src/main/java/com/project/ms/savings/fixed/accounts/MsSavingsFixedAccountsApplication.java
@@ -2,9 +2,11 @@ package com.project.ms.savings.fixed.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableReactiveMongoAuditing
 public class MsSavingsFixedAccountsApplication {
 
